2cec90b0c6aabb192c7becf67323ab793df365bc
gagarine <simon@antistatique.net>
2013-06-25 16:42:06 +0200

    capdrupal config

---
+++ config/deploy.rb
@@ -0,0 +1,29 @@
+set :application,     "explos"
+# Relative path to thedrupal path
+set :app_path,        "drupal"
+set :shared_children, ['drupal/sites/default/files']
+set :shared_files,    ['drupal/sites/default/settings.php']
+set :stages,          %w(dev prod)
+
+set :scm,            "git"
+set :repository,     "git@github.com:zufrieden/explos.git"
+
+set :domain,         "ligue.alwaysdata.net"
+
+set :user,           "ligue"
+set :group,          "ligue"
+set :runner_group,   "ligue"
+set :use_sudo,       false
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+set :download_drush, true
+
+role :app,           domain
+role :db,            domain
+
+set  :keep_releases,   3
+after "deploy:update", "deploy:cleanup" 
+
+# Be more verbose by uncommenting the following line
+#logger.level = Logger::MAX_LEVEL
---
+++ config/deploy/prod.rb
@@ -0,0 +1,6 @@
+set :stages,    "prod"
+
+set :branch,    "master"
+set :domain,    "ligue.alwaysdata.net"
+
+set :deploy_to, "/home/ligue/www/explos/"

48445fd6e22162a9ac890335eb773860eb9e92bf
Toni Fisler <mail@tonifisler.ch>
2016-04-08 10:07:43 +0200

    style the workbench info block as alert

--- drupal/sites/all/themes/ligue/template.php
+++ drupal/sites/all/themes/ligue/template.php
@@ -3,3 +3,18 @@
  * @file
  * The primary PHP file for this theme.
  */
+
+function ligue_block_view_alter(&$data, $block) {
+  if ($block->module == 'workbench') {
+    $items = module_invoke_all('workbench_block');
+    if (empty($items)) {
+      return;
+    }
+    $data = array(
+      'subject' => '',
+      'content' => array(
+        '#markup' => theme('container', array('element' => array('#children' => implode('<br />', $items), '#attributes' => array('class' => 'alert alert-info workbench-info'))))
+      ),
+    );
+  }
+}

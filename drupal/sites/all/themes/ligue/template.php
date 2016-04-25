5c0fe43a29c5491e5ffb41e9d96d13172d96796c
Toni Fisler <mail@tonifisler.ch>
2016-04-25 15:06:09 +0200

    style the activities

--- drupal/sites/all/themes/ligue/template.php
+++ drupal/sites/all/themes/ligue/template.php
@@ -35,9 +35,39 @@ function ligue_preprocess_node(&$variables) {
   }
 }
 
-function ligue_preprocess_field($variables) {
+function ligue_preprocess_field(&$variables) {
   $element = $variables['element'];
   if ($element['#field_name'] == 'field_peoples') {
     $variables['label'] = '' . $variables['label'];
   }
+
+  if ($element['#field_name'] == 'field_activity_type' || $element['#field_name'] == 'field_duration') {
+    $variables['classes_array'][] = 'flex-baseline';
+  }
+
+  if ($element['#field_name'] == 'field_lesson_stage') {
+    $label = $element[0]['#markup'];
+    $regex = "/^([0-9]\s)/";
+    $number = array();
+    preg_match($regex, $label, $number);
+    $variables['label'] = preg_replace($regex, "", $label);
+    $variables['number'] = str_replace(" ", "", $number[0]);
+  }
+
+  if ($variables['element']['#entity_type'] == 'field_collection_item') {
+    // Check if the bundle name (i.e. the field collection field name) is
+    // among the theme hook suggestions.
+    $index = array_search('field__' . $variables['element']['#bundle'],
+      $variables['theme_hook_suggestions']);
+
+    // Remove the bundle theme hook suggestion.
+    if ($index !== false) {
+      unset($variables['theme_hook_suggestions'][$index]);
+    }
+  }
+}
+
+function ligue_field_collection_view($variables) {
+  $element = $variables['element'];
+  return $element['#children'];
 }
---
+++ drupal/sites/all/themes/ligue/templates/field/field-collection/field-collection-item--field-activities.tpl.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation for field collection items.
+ *
+ * Available variables:
+ * - $content: An array of comment items. Use render($content) to print them all, or
+ *   print a subset such as render($content['field_example']). Use
+ *   hide($content['field_example']) to temporarily suppress the printing of a
+ *   given element.
+ * - $title: The (sanitized) field collection item label.
+ * - $url: Direct url of the current entity if specified.
+ * - $page: Flag for the full page state.
+ * - $classes: String of classes that can be used to style contextually through
+ *   CSS. It can be manipulated through the variable $classes_array from
+ *   preprocess functions. By default the following classes are available, where
+ *   the parts enclosed by {} are replaced by the appropriate values:
+ *   - entity-field-collection-item
+ *   - field-collection-item-{field_name}
+ *
+ * Other variables:
+ * - $classes_array: Array of html class attribute values. It is flattened
+ *   into a string within the variable $classes.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_entity()
+ * @see template_process()
+ */
+
+?>
+<div class="panel-heading" role="tab" id="heading-<?php print $id; ?>">
+  <h4 class="panel-title">
+    <a role="button" data-toggle="collapse" data-parent="#activities-accordion" href="#collapse-<?php print $id; ?>" aria-expanded="false" aria-controls="collapse-index">
+      <?php print render($content['field_title']); ?>
+    </a>
+  </h4>
+  <?php print render($content['field_duration']); ?>
+  <?php print render($content['field_activity_type']); ?>
+</div>
+<div class="panel-collapse collapse" id="collapse-<?php print $id; ?>" role="tabpanel" aria-labelledby="heading-<?php print $id; ?>">
+  <div class="panel-body">
+    <?php print render($content['field_lesson_stage']); ?>
+    <div class="spacer spacer-xs"></div>
+    <?php print render($content); ?>
+  </div>
+</div>
